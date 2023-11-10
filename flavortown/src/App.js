adf0277831410f27646556532f556a65795caa43
Sara Gregg <sara.gregg@target.com>
2023-11-10 15:12:27 -0600

    banner

---
+++ flavortown/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" >
+        <Banner />
+        
+
+      </header>
+      <body></body>
+    </div>
+  );
+}
+
+export default App;
---
+++ flavortown/src/components/Banner.js
@@ -0,0 +1,17 @@
+
+
+
+const Banner = () => {
+    const bannerStyleObj = {
+        fontSize: 72,
+        color: "blue",
+        textAlign: "left",
+        paddingTop: "60px",
+        paddingLeft: "60px",
+    }
+return (
+    <h1 style={bannerStyleObj}>Flavortown USA</h1>
+)
+}
+
+export default Banner;
