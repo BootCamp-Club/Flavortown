eb8c7b5b998e5bf14b7edd6b772bb07b889f2bbc
Sara Gregg <sara.gregg@target.com>
2024-02-02 15:08:57 -0600

    wip from feb 2

--- flavortown/src/App.js
+++ flavortown/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner.js';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" >
         <Banner />
-        
+        <NavBar data-testid={"Nav-Bar-Is-Here"} />
 
       </header>
       <body></body>
---
+++ flavortown/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div>
+        <a class="navbar-item" href="https://www.foodnetwork.com/shows/diners-drive-ins-and-dives" target="_blank">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYHa6qX-beHWiErddeP-68R4eKUwfuHtrcbLDjanJMZfHli9hl" width="180" height="300" />
+        </a>
+
+        <a class="navbar-item">
+        &nbsp;Home&nbsp;
+        </a>
+
+        <a class="navbar-item">
+                About&nbsp;
+        </a>
+
+        <a class="navbar-item">
+                Contact&nbsp;
+        </a>
+
+
+        </div>
+    </nav>
+    )}
+
+export default NavBar
